a0dd89a45e9ec90ad67d515b13bf650b8a0e0012
Chaya Flayshman <ch.f5948@gmail.com>
2021-11-30 15:00:06 +0200

    Change to physical movement and add scripts flag

--- Assets/Scripts/Flag.cs
+++ Assets/Scripts/Flag.cs
@@ -1,19 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Flag : MonoBehaviour
-{
-    //public void OnTriggerEnter(Collider other)
-    //{
-    //    if (other.gameObject.ComperTag("Player"))
-    //    {
-    //        PlayerMovement playerMovement = other.gameObject.GetComponentInChildren<PlayerMovement>();
-    //        if (playerMovement.enumname == Groupe1)
-    //        {
-    //            Debug.Log("Player 1 collided with the flag ");
-    //        }
-                
-    //    }
-    //}
+{private void OnCollisionEnter(Collision other)
+
+    {
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            PlayerMovement playerMovement = other.gameObject.GetComponentInChildren<PlayerMovement>();
+            Groups groupes = playerMovement.myGroup;
+            if (groupes == Groups.Groupe1)
+            {
+                Debug.Log("Player 1 collided with the flag ");
+            }
+
+        }
+    }
 }
--- Assets/Scripts/Players/PlayerMovement.cs
+++ Assets/Scripts/Players/PlayerMovement.cs
@@ -7,10 +7,12 @@ public enum Groups
     Groupe1,
     Groupe2
 }
+[RequireComponent(typeof(Rigidbody))]
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 10f;
+  
+    [SerializeField] private float moveSpeed = 0.15f;
     [SerializeField] private float rotateSpeed = 100f;
 
     private const string VERTICAL1_AXIS = "Vertical1";
@@ -25,38 +27,55 @@ public class PlayerMovement : MonoBehaviour
     private float v2Input;
     private float h2Input;
 
-
-
     private Rigidbody rb;
 
-    [SerializeField] private Groups myGroup;
+    public Groups myGroup;
 
     private void Start()
     {
 
         rb = GetComponent<Rigidbody>();
-
+        
     }
 
     private void Update()
     {
+        if(myGroup == Groups.Groupe1)
+        {
+                v1Input = Input.GetAxis(VERTICAL1_AXIS) * moveSpeed;
+                h1Input = Input.GetAxis(HORIZONTAL1_AXIS) * rotateSpeed;
+                
+                //transform.Translate(Vector3.forward * v1Input * Time.deltaTime);
+                //transform.Rotate(Vector3.up * h1Input * Time.deltaTime);
+            
+        
+        }
+        else if(myGroup == Groups.Groupe2)
+        {
+             v2Input = Input.GetAxis(VERTICAL2_AXIS) * moveSpeed;
+             h2Input = Input.GetAxis(HORIZONTAL2_AXIS) * rotateSpeed;
+
+            //transform.Translate(Vector3.forward * v2Input * Time.deltaTime);
+            //transform.Rotate(Vector3.up * h2Input * Time.deltaTime);
+        }
+    }
+    private void FixedUpdate()
+    {
+
         if (myGroup == Groups.Groupe1)
         {
-            v1Input = Input.GetAxis(VERTICAL1_AXIS) * moveSpeed;
-            h1Input = Input.GetAxis(HORIZONTAL1_AXIS) * rotateSpeed;
-
-            transform.Translate(Vector3.forward * v1Input * Time.deltaTime);
-            transform.Rotate(Vector3.up * h1Input * Time.deltaTime);
-
-
+            Vector3 rotation1 = Vector3.up * h1Input;
+            Quaternion angelRot1 = Quaternion.Euler(rotation1 * Time.fixedDeltaTime);
+            rb.MovePosition(transform.position + transform.forward * v1Input);
+            rb.MoveRotation(rb.rotation * angelRot1);
         }
         else if (myGroup == Groups.Groupe2)
         {
-            v2Input = Input.GetAxis(VERTICAL2_AXIS) * moveSpeed;
-            h2Input = Input.GetAxis(HORIZONTAL2_AXIS) * rotateSpeed;
-
-            transform.Translate(Vector3.forward * v2Input * Time.deltaTime);
-            transform.Rotate(Vector3.up * h2Input * Time.deltaTime);
+            Vector3 rotation2 = Vector3.up * h2Input;
+            Quaternion angelRot2 = Quaternion.Euler(rotation2 * Time.fixedDeltaTime);
+            rb.MovePosition(transform.position + transform.forward * v2Input);
+            rb.MoveRotation(rb.rotation * angelRot2);
         }
-    }
+        
+    }    
 }
