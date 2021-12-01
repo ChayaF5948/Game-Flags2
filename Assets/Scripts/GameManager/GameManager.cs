e4f6552bfc51cd639ae0169fd06ba29f5adaa7d6
ChayaF <ch.f5948@gmail.com>
2021-12-01 19:30:08 +0200

    Add gameManager Scrips

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -1,18 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GameManager : MonoBehaviour
 {
+    public UnityAction OnFlagConquered;
+
+    private bool isConquered = false;
+    public bool IsConquered
+    {
+        get { return isConquered; }
+        set
+        {
+            isConquered = value;
+            Debug.Log("The flag catch");
+            if (isConquered)
+            {
+                OnFlagConquered?.Invoke();
+                isConquered = false;
+            }
+        }
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
