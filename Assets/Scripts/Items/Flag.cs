9e78cec42c9ec349f07a5da38c63e8046e018e59
ChayaF <ch.f5948@gmail.com>
2021-12-05 19:39:03 +0200

    Change in the area script

--- Assets/Scripts/Items/Flag.cs
+++ Assets/Scripts/Items/Flag.cs
@@ -31,33 +31,22 @@ public class Flag : MonoBehaviour
             Groups groupes = playerMovement.myGroup;
             if (groupes == Groups.Groupe1 && myFlag == Groups.Groupe2)
             {
-                //transform.parent.GetComponent<Renderer>().material = area[0];
-                //transform.parent.gameObject.layer = 6; 
                 //Debug.Log("Player 1 collided with the flag ");
-                
                 gameManager.IsConqueredGro1 = true;
                 gameObject.SetActive(false);
                 flag.SetActive(true);
-                //ChangeLayer();
                 transform.parent.gameObject.layer = 6;
                 transform.parent.GetComponent<Renderer>().material = area[0];
-
             }
             else if (groupes == Groups.Groupe2 && myFlag == Groups.Groupe1)
             {
-                //transform.parent.GetComponent<Renderer>().material = area[1];
+                
                 //Debug.Log("Player 2 collided with the flag ");
                 gameManager.IsConqueredGro2 = true;
-
-                //foreach (Transform child in transform)
-                //{
-                //    child.gameObject.SetActive(true);
-
-                //}
                 gameObject.SetActive(false);
                 flag.SetActive(true);
-                //ChangeLayer();
-
+                transform.parent.gameObject.layer = 7;
+                transform.parent.GetComponent<Renderer>().material = area[1];
             }
         }
     }
