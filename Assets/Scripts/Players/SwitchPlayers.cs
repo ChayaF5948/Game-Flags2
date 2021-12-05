dfa49f1e6f44d11e1a11db099fe21d3e95b4b0cf
Gilih <gilih3479@gmail.com>
2021-12-05 10:12:47 +0200

    Add a enum for palyer's number in switchPlayers script

--- Assets/Scripts/Players/SwitchPlayers.cs
+++ Assets/Scripts/Players/SwitchPlayers.cs
@@ -2,7 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SitchPlayers : MonoBehaviour
+public enum PlayerNumber
+{
+    Player1,
+    Player2,
+    Player3,
+    Player4
+}
+public class SwitchPlayers : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
