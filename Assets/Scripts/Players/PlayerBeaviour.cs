f4645a2918714672109b9762f35c41dcb0512584
Gilih <gilih3479@gmail.com>
2021-12-05 19:42:24 +0200

    Changes in the PlayerBeaviour script
    when player collised with enemy player in his field he can catch him and prevent him from moving

--- Assets/Scripts/Players/PlayerBeaviour.cs
+++ Assets/Scripts/Players/PlayerBeaviour.cs
@@ -8,28 +8,68 @@ public class PlayerBeaviour : MonoBehaviour
 
     private PlayerMovement PlayerMovement;
 
+    public Groups myGroup;
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.collider.gameObject.layer == LayerMask.NameToLayer("AreaGroupe2"))
+        if(myGroup == Groups.Groupe2)
         {
-            isMyGround = true;
+            
+
+            if (collision.collider.gameObject.layer == LayerMask.NameToLayer("AreaGroupe2"))
+            {
+                isMyGround = true;
+
+                this.GetComponent<BoxCollider>().isTrigger = false;
+            }
+
+            else if (collision.collider.gameObject.layer == LayerMask.NameToLayer("AreaGroupe1"))
+            {
+                isMyGround = false;
+
+                this.GetComponent<BoxCollider>().isTrigger = true;
+            }
         }
 
-        else if (collision.collider.gameObject.layer == LayerMask.NameToLayer("AreaGroupe1"))
+        else if (myGroup == Groups.Groupe1)
         {
-            isMyGround = false;
+            
+
+            if (collision.collider.gameObject.layer == LayerMask.NameToLayer("AreaGroupe2"))
+            {
+                isMyGround = false;
+
+                this.GetComponent<BoxCollider>().isTrigger = true;
+            }
+
+            else if (collision.collider.gameObject.layer == LayerMask.NameToLayer("AreaGroupe1"))
+            {
+                isMyGround = true;
+                this.GetComponent<BoxCollider>().isTrigger = false;
+
+            }
         }
+
+
 
     }
     private void OnTriggerEnter(Collider other)
-    {
+    {  
+           if (myGroup == Groups.Groupe1 && isMyGround && other.gameObject.CompareTag("Group2"))
+           {
+               Debug.Log("I catched you!");
+               PlayerMovement = other.gameObject.GetComponent<PlayerMovement>();
+               PlayerMovement.enabled = false;
+           }
        
-        if (isMyGround && other.gameObject.CompareTag("Group2"))
-        {
-            Debug.Log("Hi! I'm an enemy!");
-            other.gameObject.GetComponent<PlayerMovement>();
-            PlayerMovement.enabled = false;
-        }
+           else if (myGroup == Groups.Groupe2 && isMyGround && other.gameObject.CompareTag("Group1"))
+           {
+               Debug.Log("I catched you!");
+               PlayerMovement = other.gameObject.GetComponent<PlayerMovement>();
+               PlayerMovement.enabled = false;
+           }
+       
+
+        
     }
 }
