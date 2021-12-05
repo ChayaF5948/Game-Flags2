70f1c5e3594f1b7fd6e6a5854160d81aecff6d6d
Gilih <gilih3479@gmail.com>
2021-12-05 10:41:21 +0200

    Add a refrence to the PlayerMovement script from SwitchPlayer script

--- Assets/Scripts/Players/SwitchPlayers.cs
+++ Assets/Scripts/Players/SwitchPlayers.cs
@@ -11,14 +11,30 @@ public enum PlayerNumber
 }
 public class SwitchPlayers : MonoBehaviour
 {
-    private PlayerMovement PlayerMovement;
+    private PlayerMovement Player1Movement;
+    private PlayerMovement Player2Movement;
+    private PlayerMovement Player3Movement;
+    private PlayerMovement Player4Movement;
 
     [SerializeField] PlayerNumber PlayerNumber;
+
+    private GameObject Player1;
+    private GameObject Player2;
+    private GameObject Player3;
+    private GameObject Player4;
 
     // Start is called before the first frame update
     void Start()
     {
-        PlayerMovement = GetComponent<PlayerMovement>();
+        Player1 = GameObject.Find("Player1");
+        Player2 = GameObject.Find("Player2");
+        Player3 = GameObject.Find("Player3");
+        Player4 = GameObject.Find("Player4");
+
+        Player1Movement = Player1.GetComponent<PlayerMovement>();
+        Player2Movement = Player2.GetComponent<PlayerMovement>();
+        Player3Movement = Player3.GetComponent<PlayerMovement>();
+        Player4Movement = Player4.GetComponent<PlayerMovement>();
     }
 
     // Update is called once per frame
@@ -32,6 +48,14 @@ public class SwitchPlayers : MonoBehaviour
                
             }
 
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            if (PlayerNumber != PlayerNumber.Player2)
+            {
+               
+            }
         }
     }
 }
