303f1984761aa86444375ef8b9068a1bd455cfd4
ChayaF <ch.f5948@gmail.com>
2021-12-05 19:28:52 +0200

    Add Layers And code that change the layer wham tha flag is Conquered

--- Assets/Scripts/Items/Flag.cs
+++ Assets/Scripts/Items/Flag.cs
@@ -6,13 +6,18 @@ public class Flag : MonoBehaviour
     private Groups myFlag;
     [SerializeField]
     private GameObject flag;
+
     private GameManager gameManager;
+
     [SerializeField]
     private Material[] area;
-    [SerializeField] private LayerMask areaMask;
+    
+
+    //[SerializeField] private LayerMask areaMask;
     private void Start()
     {
         gameManager = GameManager.FindObjectOfType<GameManager>();
+        //areaMask = GetComponentInParent<LayerMask>();
     }
 
 
@@ -26,18 +31,22 @@ public class Flag : MonoBehaviour
             Groups groupes = playerMovement.myGroup;
             if (groupes == Groups.Groupe1 && myFlag == Groups.Groupe2)
             {
-                transform.parent.GetComponent<Renderer>().material = area[0];
-                //transform.parent.layer = 7;
-                Debug.Log("Player 1 collided with the flag ");
-                //gameObject.SetActive = false;
+                //transform.parent.GetComponent<Renderer>().material = area[0];
+                //transform.parent.gameObject.layer = 6; 
+                //Debug.Log("Player 1 collided with the flag ");
+                
                 gameManager.IsConqueredGro1 = true;
                 gameObject.SetActive(false);
                 flag.SetActive(true);
+                //ChangeLayer();
+                transform.parent.gameObject.layer = 6;
+                transform.parent.GetComponent<Renderer>().material = area[0];
+
             }
             else if (groupes == Groups.Groupe2 && myFlag == Groups.Groupe1)
             {
-                transform.parent.GetComponent<Renderer>().material = area[1];
-                Debug.Log("Player 2 collided with the flag ");
+                //transform.parent.GetComponent<Renderer>().material = area[1];
+                //Debug.Log("Player 2 collided with the flag ");
                 gameManager.IsConqueredGro2 = true;
 
                 //foreach (Transform child in transform)
@@ -47,8 +56,23 @@ public class Flag : MonoBehaviour
                 //}
                 gameObject.SetActive(false);
                 flag.SetActive(true);
+                //ChangeLayer();
 
             }
         }
     }
+    //private void ChangeLayer()
+    //{
+    //    //LayerMask areas = transform.parent.GetComponent<LayerMask>();
+        //if(areas == LayerMask.NameToLayer("AraeGroup1"))
+        //{
+        //    transform.parent.gameObject.layer = 7;
+        //    transform.parent.GetComponent<Renderer>().material = area[1];
+        //}
+        //else if (areas == LayerMask.NameToLayer("AraeGroup2"))
+        //{
+        //    transform.parent.gameObject.layer = 6;
+        //    transform.parent.GetComponent<Renderer>().material = area[1];
+        //}
+    //}
 }
