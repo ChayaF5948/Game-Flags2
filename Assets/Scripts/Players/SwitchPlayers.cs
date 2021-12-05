c817ab311b290604ef8e544cba46021a047b3086
Gilih <gilih3479@gmail.com>
2021-12-05 14:46:28 +0200

    Changes in SwitchPlayers script

--- Assets/Scripts/Players/SwitchPlayers.cs
+++ Assets/Scripts/Players/SwitchPlayers.cs
@@ -2,60 +2,73 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum PlayerNumber
-{
-    Player1,
-    Player2,
-    Player3,
-    Player4
-}
+
 public class SwitchPlayers : MonoBehaviour
 {
+
     private PlayerMovement Player1Movement;
     private PlayerMovement Player2Movement;
     private PlayerMovement Player3Movement;
     private PlayerMovement Player4Movement;
 
-    [SerializeField] PlayerNumber PlayerNumber;
+    PlayerNumber PlayerNumber;
 
-    private GameObject Player1;
-    private GameObject Player2;
-    private GameObject Player3;
-    private GameObject Player4;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        Player1 = GameObject.Find("Player1");
-        Player2 = GameObject.Find("Player2");
-        Player3 = GameObject.Find("Player3");
-        Player4 = GameObject.Find("Player4");
+        Player1Movement = GameObject.Find("Player1").GetComponent<PlayerMovement>();
+        Player2Movement = GameObject.Find("Player2").GetComponent<PlayerMovement>();
+        Player3Movement = GameObject.Find("Player3").GetComponent<PlayerMovement>();
+        Player4Movement = GameObject.Find("Player4").GetComponent<PlayerMovement>();
 
-        Player1Movement = Player1.GetComponent<PlayerMovement>();
-        Player2Movement = Player2.GetComponent<PlayerMovement>();
-        Player3Movement = Player3.GetComponent<PlayerMovement>();
-        Player4Movement = Player4.GetComponent<PlayerMovement>();
+        Player1Movement.enabled = true;
+        Player2Movement.enabled = false;
+        Player3Movement.enabled = false;
+        Player4Movement.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+       
         if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-
-            if (PlayerNumber != PlayerNumber.Player1)
-            {
-               
-            }
+        {            
+                Player1Movement.enabled = true;
+                Player2Movement.enabled = false;
+                Player3Movement.enabled = false;
+                Player4Movement.enabled = false;
 
         }
 
         else if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            if (PlayerNumber != PlayerNumber.Player2)
-            {
-               
-            }
+            
+            Player1Movement.enabled = false;
+            Player2Movement.enabled = true;
+            Player3Movement.enabled = false;
+            Player4Movement.enabled = false;
         }
+
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+
+            Player1Movement.enabled = false;
+            Player2Movement.enabled = false;
+            Player3Movement.enabled = true;
+            Player4Movement.enabled = false;
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+
+            Player1Movement.enabled = false;
+            Player2Movement.enabled = false;
+            Player3Movement.enabled = false;
+            Player4Movement.enabled = true;
+        }
+
+
     }
 }
