1c953ff30ec98ed84d80529ec90b7f45acd6c229
ChayaF <ch.f5948@gmail.com>
2021-12-05 14:38:20 +0200

    Add  change -  What happened when the player collided with a flag

--- Assets/Scripts/Items/Flag.cs
+++ Assets/Scripts/Items/Flag.cs
@@ -26,17 +26,17 @@ public class Flag : MonoBehaviour
             Groups groupes = playerMovement.myGroup;
             if (groupes == Groups.Groupe1 && myFlag == Groups.Groupe2)
             {
-                transform.parent.GetComponent<Renderer>().material = area[1];
-                transform.parent.layer = 7;
+                transform.parent.GetComponent<Renderer>().material = area[0];
+                //transform.parent.layer = 7;
                 Debug.Log("Player 1 collided with the flag ");
                 //gameObject.SetActive = false;
                 gameManager.IsConqueredGro1 = true;
-                //gameObject.SetActive(false);
-                //flag.SetActive(true);
+                gameObject.SetActive(false);
+                flag.SetActive(true);
             }
             else if (groupes == Groups.Groupe2 && myFlag == Groups.Groupe1)
             {
-                transform.parent.GetComponent<Renderer>().material = area[0];
+                transform.parent.GetComponent<Renderer>().material = area[1];
                 Debug.Log("Player 2 collided with the flag ");
                 gameManager.IsConqueredGro2 = true;
 
@@ -45,8 +45,8 @@ public class Flag : MonoBehaviour
                 //    child.gameObject.SetActive(true);
 
                 //}
-                //gameObject.SetActive(false);
-                //flag.SetActive(true);
+                gameObject.SetActive(false);
+                flag.SetActive(true);
 
             }
         }
