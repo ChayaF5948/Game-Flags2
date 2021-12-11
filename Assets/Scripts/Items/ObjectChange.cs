7c877812cd37b35e117b9c2fb27106114aa455a3
ChayaF <ch.f5948@gmail.com>
2021-12-12 01:22:27 +0200

    Change in the change object

--- Assets/Scripts/Items/ObjectChange.cs
+++ Assets/Scripts/Items/ObjectChange.cs
@@ -2,18 +2,18 @@ using UnityEngine;
 
 public class ObjectChange : MonoBehaviour
 {
-    [SerializeField]
-    private GameManager gameManager;
-    [SerializeField]
-    private GameObject[] flags;
-    private void Start()
-    {
-        gameManager.OnFlagConquered += ChangeFieldAndFlag;
-    }
-    private void ChangeFieldAndFlag()
-    {
+    //[SerializeField]
+    //private GameManager gameManager;
+    //[SerializeField]
+    //private GameObject[] flags;
+    //private void Start()
+    //{
+    //    gameManager.OnFlagConquered += ChangeFieldAndFlag;
+    //}
+    //private void ChangeFieldAndFlag()
+    //{
 
-        Debug.Log("The field change");
+    //    Debug.Log("The field change");
 
-    }
+    //}
 }
