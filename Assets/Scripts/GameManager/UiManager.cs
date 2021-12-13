c2106c59af6ec222862b57da26d24274e592d955
ChayaF <ch.f5948@gmail.com>
2021-12-13 20:44:51 +0200

    Add UiManeger script

---
+++ Assets/Scripts/GameManager/UiManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiManager : MonoBehaviour
+{
+    private GameManager gameManager;
+    
+    [SerializeField]
+    private Text flagTextP1;
+    [SerializeField]
+    private Text flagTextP2;
+
+    private void Start()
+    {
+        gameManager = GameManager.FindObjectOfType<GameManager>();
+        gameManager.OnFlagConquered += ChangeTheNumberFlagsText;
+    }
+
+    private void ChangeTheNumberFlagsText(int flagsP1,int flagsP2)
+    {
+        flagTextP1.text = $"flags:{flagsP1.ToString()}";
+        flagTextP2.text = $"flags:{flagsP2.ToString()}";
+    }
+}
