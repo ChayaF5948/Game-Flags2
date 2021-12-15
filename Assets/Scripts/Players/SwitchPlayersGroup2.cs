7fd7a8e76dbb8d55e05a9e392112e82c9e9c170f
Gilih <gilih3479@gmail.com>
2021-12-15 19:33:19 +0200

    Divide the SwitchPlayers script to two seperate scripts
    SwitchPlayersGroup1 for Group1 and SwitchPlayersGroup2 for Group2

--- Assets/Scripts/Players/SwitchPlayersGroup2.cs
+++ Assets/Scripts/Players/SwitchPlayersGroup2.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-public class SwitchPlayers : MonoBehaviour
+public class SwitchPlayersGroup2 : MonoBehaviour
 {
+
     [SerializeField]
     private PlayerNumber playerNumber;
     private PlayerMovement playerMovement;
@@ -44,7 +44,7 @@ public class SwitchPlayers : MonoBehaviour
         }
 
 
-       
+
     }
 
     // Update is called once per frame
@@ -139,74 +139,4 @@ public class SwitchPlayers : MonoBehaviour
         }
     }
 
-        
-
-
-
-
-    //private PlayerMovement Player1Movement;
-    //private PlayerMovement Player2Movement;
-    //private PlayerMovement Player3Movement;
-    //private PlayerMovement Player4Movement;
-
-    //PlayerNumber PlayerNumber;
-
-
-
-    //// Start is called before the first frame update
-    //void Start()
-    //{
-    //    Player1Movement = GameObject.Find("Player_1").GetComponent<PlayerMovement>();
-    //    Player2Movement = GameObject.Find("Player_2").GetComponent<PlayerMovement>();
-    //    Player3Movement = GameObject.Find("Player_3").GetComponent<PlayerMovement>();
-    //    Player4Movement = GameObject.Find("Player_4").GetComponent<PlayerMovement>();
-
-    //    Player1Movement.enabled = true;
-    //    Player2Movement.enabled = false;
-    //    Player3Movement.enabled = false;
-    //    Player4Movement.enabled = false;
-    //}
-
-    //// Update is called once per frame
-    //void Update()
-    //{
-
-    //    if (Input.GetKeyDown(KeyCode.Alpha1))
-    //    {            
-    //            Player1Movement.enabled = true;
-    //            Player2Movement.enabled = false;
-    //            Player3Movement.enabled = false;
-    //            Player4Movement.enabled = false;
-
-    //    }
-
-    //    else if (Input.GetKeyDown(KeyCode.Alpha2))
-    //    {
-
-    //        Player1Movement.enabled = false;
-    //        Player2Movement.enabled = true;
-    //        Player3Movement.enabled = false;
-    //        Player4Movement.enabled = false;
-    //    }
-
-    //    else if (Input.GetKeyDown(KeyCode.Alpha3))
-    //    {
-
-    //        Player1Movement.enabled = false;
-    //        Player2Movement.enabled = false;
-    //        Player3Movement.enabled = true;
-    //        Player4Movement.enabled = false;
-    //    }
-
-    //    else if (Input.GetKeyDown(KeyCode.Alpha4))
-    //    {
-
-    //        Player1Movement.enabled = false;
-    //        Player2Movement.enabled = false;
-    //        Player3Movement.enabled = false;
-    //        Player4Movement.enabled = true;
-    //    }
-
-
-    //}
 }
